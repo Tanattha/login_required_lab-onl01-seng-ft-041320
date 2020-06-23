74c548e7fae2e589bda9344410ca833921aafe8c
Tanattha <tanattha.thuenim@gmail.com>
2020-06-22 20:01:03 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+    before_action :logged_in?
+    skip_before_action :logged_in?, only: [:index]
+
+    def new
+    end
+
+    def create
+        redirect_to login_path if params[:name].blank?
+        session[:name] = params[:name]
+    end
+
+    def destroy
+        session.clear if current_user
+    end
+  end
